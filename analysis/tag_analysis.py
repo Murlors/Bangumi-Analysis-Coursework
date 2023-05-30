af42d8410c98d80b84269c1d4799055ca3b96e09
Murlors <1595258509@qq.com>
2023-05-30 16:01:17 +0800

    ✨ feat(analysis): add tag_analysis.

---
+++ analysis/tag_analysis.py
@@ -0,0 +1,116 @@
+import collections
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import seaborn as sns
+
+
+class TagAnalysis:
+    def __init__(self, file_path, save_path="figures"):
+        self.data = pd.read_csv(file_path)
+        self.data["tags"] = self.data["tags"].apply(eval)
+        self.data["year"] = self.data["date"].apply(lambda x: int(x.split("-")[0]))
+        self.save_path = save_path
+
+    def count_tag_frequency(self, min_count):
+        """
+        统计每个tag的数量, 并返回一个Counter对象, 仅统计选择量>=min_count的tag
+        """
+        tag_counts = collections.Counter(
+            tag
+            for tags in self.data["tags"]
+            for tag, count in tags.items()
+            if count >= min_count
+        )
+        return tag_counts
+
+    def plot_tag_counts(self, tag_counts, top_n):
+        """
+        使用竖向柱状图降序展示前top_n个tag的数量
+        """
+        most_common = dict(tag_counts.most_common(top_n))
+        plt.barh(
+            list(most_common.keys()),
+            list(most_common.values()),
+            color="steelblue",
+            alpha=0.8,
+        )
+        plt.title("tag数量统计")
+        plt.xlabel("数量")
+        plt.ylabel("tag")
+        plt.savefig(os.path.join(self.save_path, "tag_counts.png"))
+        plt.clf()
+
+    def generate_wordcloud(self, tag_counts):
+        """
+        使用词云图展示tag的词频
+        """
+        wordcloud = WordCloud(
+            background_color="white",
+            max_words=500,
+            font_path="msyh.ttc",
+            width=1920,
+            height=1080,
+            max_font_size=500,
+        ).generate_from_frequencies(tag_counts)
+        wordcloud.to_file(os.path.join(self.save_path, "tag_wordcloud.png"))
+
+    def count_year_music(self):
+        """
+        统计每年的音乐数量, 并返回一个Series对象
+        """
+        year_counts = self.data.groupby("year").size()
+        return year_counts
+
+    def plot_year_music_trend(self, year_counts):
+        """
+        使用折线图展示每年的优秀音乐数量
+        """
+        year_counts.plot()
+        plt.xlabel("年份")
+        plt.ylabel("优秀音乐数量")
+        plt.savefig(os.path.join(self.save_path, "year_music_trend.png"))
+        plt.clf()
+
+    def count_tag_year_frequency(self, min_count=100, top_n=30):
+        """
+        计算不同年份和不同tag之间的关系, 并返回一个DataFrame对象
+        """
+        tag_year_counts = (
+            pd.json_normalize(self.data["tags"])
+            .fillna(0)
+            .astype(int)
+            .assign(year=self.data["year"])
+            .groupby("year")
+            .sum()
+            .T.loc[lambda df: df.sum(axis=1) > min_count]
+            .sort_values(by=self.data["year"].max(), ascending=False)
+            .iloc[:top_n]
+        )
+        return tag_year_counts
+
+    def plot_tag_year_counts_heatmap(self, tag_year_counts):
+        """
+        使用热力图展示不同年份和不同tag之间的关系
+        """
+        sns.heatmap(tag_year_counts, cmap="Blues", xticklabels=True)
+        plt.xlabel("年份")
+        plt.ylabel("tag")
+        plt.savefig(os.path.join(self.save_path, "tag_year_counts_heatmap.png"))
+        plt.clf()
+
+
+if __name__ == "__main__":
+    tag_analysis = TagAnalysis("data/music_infos.csv")
+
+    tag_counts = tag_analysis.count_tag_frequency(10)
+    tag_analysis.plot_tag_counts(tag_counts, 20)
+
+    tag_analysis.generate_wordcloud(tag_counts)
+
+    year_counts = tag_analysis.count_year_music()
+    tag_analysis.plot_year_music_trend(year_counts)
+
+    tag_year_counts = tag_analysis.count_tag_year_frequency(100, 30)
+    tag_analysis.plot_tag_year_counts_heatmap(tag_year_counts)
--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import argparse
 import os
+from matplotlib import pyplot as plt
 
 import yaml
 
 import crawler
+import analysis
 
 
 def get_hparams():
     parser = argparse.ArgumentParser()
     # use local saved info? (local path)
-    parser.add_argument('-l', '--local', action='store_true', help='是否使用本地保存的信息')
-    parser.add_argument('-p', '--path', type=str, default='data', help='本地保存的信息路径')
+    parser.add_argument("-l", "--local", action="store_true", help="是否使用本地保存的信息")
+    parser.add_argument("-p", "--path", type=str, help="本地保存的信息路径")
+    parser.add_argument("-fig", "--figure", type=str, help="本地保存的图片路径")
+    parser.add_argument("-t", "--type", type=str, help="爬取的条目类型")
 
-    parser.add_argument('-cfg', '--config', type=str, help='配置文件路径')
+    parser.add_argument("-cfg", "--config", type=str,default='config.yml', help="配置文件路径")
 
-    parser.add_argument('-t', '--type', type=str, help='爬取的条目类型')
-    parser.add_argument('-s', '--start', type=int, help='爬取的开始页数')
-    parser.add_argument('-e', '--end', type=int, help='爬取的结束页数')
-    parser.add_argument('-ua', '--user-agent', type=str, help='User-Agent')
+    parser.add_argument("-s", "--start", type=int, help="爬取的开始页数")
+    parser.add_argument("-e", "--end", type=int, help="爬取的结束页数")
+    parser.add_argument("-ua", "--user-agent", type=str, help="User-Agent")
 
     return parser
 
 
 def get_config(config_path):
-    with open(config_path, 'r') as f:
+    with open(config_path, "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     return config
 
@@ -32,31 +35,60 @@ def parse_args(parser):
     args = parser.parse_args()
     if args.config and os.path.exists(args.config):
         config = get_config(args.config)
-        args.type = config['crawler']['type']
-        args.start = config['crawler']['start']
-        args.end = config['crawler']['end']
-        args.user_agent = config['crawler']['user-agent']
-        args.path = config['data']['path']
+        args.type = config["crawler"]["type"]
+        args.start = config["crawler"]["start"]
+        args.end = config["crawler"]["end"]
+        args.user_agent = config["crawler"]["user-agent"]
+        args.path = config["data"]["path"]
+        args.figure = config["figure"]["path"]
+        args.rcParams = config["figure"]["rcParams"]
+    else:
+        args.type = "music"
+        args.start = 1
+        args.end = 1
+        args.path = "data"
+        args.figure = "figure"
+        args.rcParams = {
+            "font.family": "SimHei",
+            "savefig.dpi": 300,
+            "figure.figsize": [12, 8],
+        }
+    if not os.path.exists(args.path):
+        os.makedirs(args.path)
+    if not os.path.exists(args.figure):
+        os.makedirs(args.figure)
     return args
 
 
 def main():
     parser = get_hparams()
     args = parse_args(parser)
+    plt.rcParams.update(args.rcParams)
 
-    if args.local:
-        # use local saved info
-        pass
-    else:
+    if not args.local:
         # get music subject codes
         rank_crawler = crawler.RankCrawler(args.type, args.path, args.start, args.end)
         subject_codes = rank_crawler.get_subject_codes()
         # get music subject info
-        headers = {
-            'User-Agent': args.user_agent
-        }
+        headers = {"User-Agent": args.user_agent}
         music_crawler = crawler.MusicCrawler(args.path, headers)
         music_infos = music_crawler.get_music_info(subject_codes)
 
-if __name__ == '__main__':
+    tag_analysis = analysis.TagAnalysis(
+        os.path.join(args.path, f"{args.type}_infos.csv")
+    )
+
+    tag_counts = tag_analysis.count_tag_frequency(min_count=10)
+    tag_analysis.plot_tag_counts(tag_counts, top_n=20)
+
+    tag_analysis.generate_wordcloud(tag_counts)
+
+    year_counts = tag_analysis.count_year_music()
+    tag_analysis.plot_year_music_trend(year_counts)
+
+    tag_year_counts = tag_analysis.count_tag_year_frequency(min_count=100, top_n=30)
+    tag_analysis.plot_tag_year_counts_heatmap(tag_year_counts)
+
+
+if __name__ == "__main__":
     main()
