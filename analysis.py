ba312aac435e44b8ca36f40dde9f15ab05e4ebe7
Murlors <1595258509@qq.com>
2023-05-31 18:15:14 +0800

    🎈 perf: use `yaml.safe_load`.

--- analysis.py
+++ analysis.py
@@ -23,7 +23,7 @@ def get_hparams():
 
 def get_config(config_path):
     with open(config_path, "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
+        config = yaml.safe_load(f)
     return config
 
 
